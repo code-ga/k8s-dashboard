909e3e8c0def71cdd1712065f123de10240be2a7
Code-ga <tritranduc11@gmail.com>
2026-02-22 23:01:23 +0700

    abc

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -1,7 +1,8 @@
 export const BACKEND_URL =
-	import.meta.env.VITE_BACKEND_URL || import.meta.env.DEV
+	import.meta.env.VITE_BACKEND_URL ||
+	(import.meta.env.DEV
 		? "http://localhost:3001"
-		: "https://the-bridge-backend.nbth.id.vn";
+		: "https://the-bridge-backend.nbth.id.vn");
 export const FRONTEND_URL =
 	import.meta.env.VITE_APP_URL || "http://localhost:3000";
 console.log(FRONTEND_URL);
