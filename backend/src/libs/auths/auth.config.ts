36fcfe756f6e71b14cfaed37a4abe0066976316f
Code-ga <tritranduc11@gmail.com>
2026-04-30 13:36:55 +0700

    uhh idk

--- backend/src/libs/auths/auth.config.ts
+++ backend/src/libs/auths/auth.config.ts
@@ -54,7 +54,7 @@ export const auth = betterAuth({
 		// 		attributes: cookieOptions,
 		// 	},
 		// },
-		defaultCookieAttributes: cookieOptions,
+		// defaultCookieAttributes: cookieOptions,
 		crossSubDomainCookies: {
 			enabled: true,
 		},
